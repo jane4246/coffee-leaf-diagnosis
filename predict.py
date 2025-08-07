86fa6b32fce1c1d82ba189b9fb1c2a332836f9d3
maritimjane <58557449+jane4246@users.noreply.github.com>
2025-08-07 07:11:38 -0800

    Update predict.py

--- predict.py
+++ predict.py
@@ -36,3 +36,7 @@ def predict():
         return jsonify({'prediction': predicted_class, 'confidence': confidence})
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0', port=10000)
+
